29cb3a3c382fb4b9eba84fb79884225a48ba02a6
Aden1ke <sarahogunlalu@gmail.com>
2023-06-26 12:25:21 -0700

    betty

--- prints_integer.c
+++ prints_integer.c
@@ -8,30 +8,30 @@
  *
  * Return: (Lengt -1)
  */
-int _prints_integer(int numb)                     {
-        int lengt1 = 0;
-        int lengt2 = 0;
-                                                          if (!numb)
-        {
-                lengt2 += _putchar('0');
-                return (lengt2);
+int _prints_integer(int numb)
+{
+	int lengt1 = 0;
+	int lengt2 = 0;
+
+	if (!numb)
+	{
+		lengt2 += _putchar('0');
+		return (lengt2);
 	}
-							  if (numb <= -1)
+	if (numb <= -1)
 	{
 		lengt2 += _putchar('-');
 		numb = -numb;
 	}
-							  if (numb == -2147483648)
+	if (numb == -2147483648)
 	{
 		lengt2 += print_string("-2147483648");
 		return (lengt2);
 	}
-							  lengt1 = numb % 10;
+	lengt1 = numb % 10;
 	numb = numb / 10;
 	if (numb)
 		lengt2 += _prints_integer(numb);
 	lengt2 += _putchar(lengt1 + '0');
-
 	return (lengt2);
 }
-
